62479260aae20d06dbedfe14b225d074066dbcd3
WhosDaMacc <142145705+WhosDaMacc@users.noreply.github.com>
2025-02-02 23:34:16 -0800

    Update profitpool.sol

--- contracts/profitpool.sol
+++ contracts/profitpool.sol
@@ -51,3 +51,28 @@ contract ProfitPool {
         }
     }
 }
+// contracts/ProfitPool.sol
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.20;
+
+contract ProfitPool {
+    address[] public users;
+    mapping(address => uint) public shares;
+    uint public totalShares;
+    
+    // Add user with percentage share (1 = 1%)
+    function addUser(address user, uint percentage) external {
+        require(totalShares + percentage <= 100, "Exceeds 100%");
+        users.push(user);
+        shares[user] = percentage;
+        totalShares += percentage;
+    }
+
+    // Distribute ETH to users proportionally
+    function distribute() external payable {
+        for(uint i; i < users.length; i++) {
+            uint amount = (msg.value * shares[users[i]]) / 100;
+            payable(users[i]).transfer(amount);
+        }
+    }
+}
