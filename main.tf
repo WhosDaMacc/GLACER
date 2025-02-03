b02e77d93df3e1102acf9df7891f1194dac421ae
WhosDaMacc <142145705+WhosDaMacc@users.noreply.github.com>
2025-02-03 08:34:32 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
-    organization = "example-organization"
+    organization = "GLACERWORLD"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-      name = "example-workspace"
+      name = "GlacerIceberg"
     }
   }
 }
