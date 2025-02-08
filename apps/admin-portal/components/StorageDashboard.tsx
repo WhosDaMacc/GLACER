99c2bea8e194bada7c9208dde95360ba77f0568d
WhosDaMacc <142145705+WhosDaMacc@users.noreply.github.com>
2025-02-07 17:34:56 -0800

    Update StorageDashboard.tsx

--- apps/admin-portal/components/StorageDashboard.tsx
+++ apps/admin-portal/components/StorageDashboard.tsx
@@ -1,25 +1,16 @@
 import { useStorageMetrics } from '../hooks/useStorageMetrics';
+import TierDistributionChart from './TierDistributionChart';
+import IntegrityHealth from './IntegrityHealth';
+import BlockchainSyncStatus from './BlockchainSyncStatus';
 
 export default function StorageDashboard() {
   const { data } = useStorageMetrics();
-  
+
   return (
     <div className="dashboard">
-      <TierDistributionChart 
-        hot={data.hot}
-        warm={data.warm}
-        cold={data.cold}
-      />
-      
-      <IntegrityHealth 
-        verified={data.verifiedFiles}
-        tampered={data.tamperedFiles}
-      />
-      
-      <BlockchainSyncStatus 
-        lastBlock={data.lastBlock}
-        syncLag={data.syncLag}
-      />
+      <TierDistributionChart hot={data.hot} warm={data.warm} cold={data.cold} />
+      <IntegrityHealth verified={data.verifiedFiles} tampered={data.tamperedFiles} />
+      <BlockchainSyncStatus lastBlock={data.lastBlock} syncLag={data.syncLag} />
     </div>
   );
 }
